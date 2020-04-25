94bff51286a9b347a090646447b2180567be86fd
Jr Sarath <jrsarath@outlook.com>
2020-04-25 15:13:14 +0530

    Version 2.0

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*
+    Jr Creative - main.js
+    Copyright © 2020 - Jr Sarath
+    https://github.com/jrsarath
+*/
+
+$(document).ready(() => {
+    setTimeout(() => {
+       $(".loader").fadeOut('slow'); 
+    }, 5000);
+})

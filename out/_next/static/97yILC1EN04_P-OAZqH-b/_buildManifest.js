e951a4050ee2e40420e1f8b75ba6deac6ca233c6
Jr Sarath <jrsarath@outlook.com>
2022-03-29 00:18:47 +0530

    clean out

--- out/_next/static/97yILC1EN04_P-OAZqH-b/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-579d5b3125e98576.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

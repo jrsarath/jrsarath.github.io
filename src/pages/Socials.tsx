4505f557a0b0878af314ec1e121dba0a900384eb
jrsarath <jrsarath@outlook.com>
2024-07-25 03:04:59 +0530

    Deploy Github Pages

---
+++ src/pages/Socials.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Aura: React.FC = () => {
+    const handleOnClick = () => {
+        window.open("mailto:jrsarath@outlook.com")
+    }
+    return (
+        <div className="wrapper">
+            <div className="name" onClick={handleOnClick}>
+                <h1 className="content">Sarath Singh</h1>
+                <h1 className="overlay">DELTA</h1>
+            </div>
+        </div>
+    )
+}
+export default Aura;

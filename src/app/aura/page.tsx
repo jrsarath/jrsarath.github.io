92fbf3774065de580d4897ee0ff2ab89d7ba0a27
jrsarath <jrsarath@outlook.com>
2024-07-26 23:34:05 +0530

    add back old design

---
+++ src/app/aura/page.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from "next";
+import Image from 'next/image'
+
+const Aura: NextPage = () => {
+    return (
+        <div className="w-full h-full flex cursor-crosshair justify-center items-center align-center bg-[#f9f9f9] dark:bg-[#212121] select-none">
+            <div className="relative flex flex-col justify-center items-center content-center">
+                <Image
+                    width={0}
+                    height={0}
+                    layout="responsive"
+                    src="images/aura.svg"
+                    alt="Aura"
+                    className="w-full dark:invert"
+                />
+                <h1 className="mt-4 text-black font-light text-[2rem] dark:text-white">Check back later</h1>
+            </div>
+        </div>
+    )
+}
+export default Aura;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        kumarOne: ["var(--font-kumar-one)"]
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
